c64aea7258606d72ac915031e8b7fa23102b9525
seungchan.moon <moon.unimewo@gmail.com>
2022-10-01 16:27:19 +0900

    feat : AOP v3, v4, v5 적용
    v3: 어드바이스 추가, v4: 포인트컷을 모듈 처리하고 참조, v5: aop 적용 순서 제어

--- aop/src/test/java/hello/aop/AopTest.java
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -2,8 +2,7 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
-import hello.aop.order.aop.AspectV1;
-import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.*;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@Import(AspectV2.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
 @SpringBootTest
 public class AopTest {
 
